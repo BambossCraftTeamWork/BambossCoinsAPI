a3f05a7f95cf73be5e7d797b49e47a3700e78dae
Bosternike <aav0918@gmail.com>
2022-08-24 14:45:42 +0200

    777

---
+++ src/main/java/net/boster/bamboss/coins/BambossCoinsAPI.java
@@ -0,0 +1,23 @@
+package net.boster.bamboss.coins;
+
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class BambossCoinsAPI {
+
+    public static void earnCoins(@NotNull Player p, int amount) {
+    }
+
+    public static void addCoins(@NotNull Player p, int amount) {
+    }
+
+    public static void withdrawCoins(@NotNull Player p, int amount) {
+    }
+
+    public static void setCoins(@NotNull Player p, int amount) {
+    }
+
+    public static int getCoins(@NotNull Player p) {
+        return 0;
+    }
+}
